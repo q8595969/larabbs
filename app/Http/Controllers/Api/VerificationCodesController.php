ced817f232c98e1841e4496142133460f0478f1d
vagrant <vagrant@homestead>
2020-05-29 08:22:57 +0000

    发送短信验证码

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use App\Http\Requests\Api\VerificationCodeRequest;
+class VerificationCodesController extends Controller
+{
+  public function store(VerificationCodeRequest $request)
+    {
+        $phone = $request->phone;
+
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        }
+
+        $key = 'verificationCode_'.Str::random(15);
+        $expiredAt = now()->addMinutes(5);
+        // 缓存验证码 5 分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return response()->json([
+        	'code'=> $code,
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,15 +18,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::prefix('v2')->name('api.v2.')->group(function() {
-    Route::get('version', function() {
-        return 'this is version v2';
-    })->name('version');
-});
+// Route::prefix('v2')->name('api.v2.')->group(function() {
+//     Route::get('version', function() {
+//         return 'this is version v2';
+//     })->name('version');
+// });
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
     Route::get('version', function() {
-        abort(403, 'test');
         return 'this is version v1';
     })->name('version');
+
+        // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
 });
